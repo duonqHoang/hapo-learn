80ba2c07287f99d852f7cd638d1c781e4db2c016
duonqHoang <duonghoang.v06@gmail.com>
2023-08-15 15:08:23 +0700

    finished sign up screen

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,27 +1,56 @@
 import { Form } from "react-bootstrap";
 import "./SignUp.scss";
+import { useRef, useState } from "react";
 
 export default function SignUp() {
+  const [validated, setValidated] = useState(false);
+  const password = useRef();
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div className="sign-up-page">
       <div className="sign-up-container">
         <span className="sign-up-title">Sign up to HapoLearn</span>
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label>Username</Form.Label>
-            <Form.Control />
+            <Form.Control required />
           </Form.Group>
           <Form.Group>
             <Form.Label>Email</Form.Label>
-            <Form.Control />
+            <Form.Control type="email" required />
           </Form.Group>
           <Form.Group>
             <Form.Label>Password</Form.Label>
-            <Form.Control />
+            <Form.Control
+              name="password"
+              type="password"
+              ref={password}
+              required
+            />
           </Form.Group>
           <Form.Group>
             <Form.Label>Confirm Password</Form.Label>
-            <Form.Control />
+            <Form.Control
+              name="passwordConfirm"
+              type="password"
+              onChange={(e) => {
+                e.target.setCustomValidity(
+                  password.current.value === e.target.value ? "" : "invalid"
+                );
+              }}
+              required
+            />
           </Form.Group>
           <button className="sign-up-btn" type="submit">
             Sign up
