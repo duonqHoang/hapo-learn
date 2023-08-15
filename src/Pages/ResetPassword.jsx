e23e0e37dde6c89ec892f20d6664525fca360726
duonqHoang <duonghoang.v06@gmail.com>
2023-08-15 15:40:38 +0700

    fix styling

--- src/Pages/ResetPassword.jsx
+++ src/Pages/ResetPassword.jsx
@@ -1,7 +1,21 @@
 import { Form } from "react-bootstrap";
 import "./ResetPassword.scss";
+import { useState } from "react";
 
 export default function ResetPassword() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div className="reset-password-page">
       <div className="reset-password-container">
@@ -9,10 +23,13 @@ export default function ResetPassword() {
         <div className="reset-password-sub">
           Enter email to reset your password
         </div>
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label>Email:</Form.Label>
             <Form.Control type="email" required />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid email address!
+            </Form.Control.Feedback>
           </Form.Group>
           <button className="submit-btn" type="submit">
             RESET PASSWORD
